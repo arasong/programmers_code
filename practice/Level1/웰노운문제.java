b7ad928dfe737e5939cdb601aa1f046607b349eb
송아라 <a2041420@gmail.com>
2023-02-25 23:53:39 +0900

    풀이 update

--- practice/Level1/웰노운문제.java
+++ practice/Level1/웰노운문제.java
@@ -1,10 +1,11 @@
 package Level1;
 
 import java.util.*;
+import java.util.Map.Entry;
 
 public class 웰노운문제 {
     public static void main(String[] args) {
-        //      String[] answer = solution(new String[]{"kate sqrt"});
+        //String[] answer = solution(new String[]{"kate sqrt"});
         String[] answer = solution(new String[]{"morgan sort", "felix sort", "morgan sqrt", "morgan sqrt", "rohan reverse", "rohan reverse"});
         //String[] answer = solution(new String[]{"morgan string_compare", "felix string_compare", "morgan reverse", "rohan sort", "andy reverse", "morgan sqrt"});
         for (String arg : answer) {
@@ -20,35 +21,40 @@ public class 웰노운문제 {
         HashSet<String> users = new HashSet<>();
         HashMap<String, Integer> hm = new HashMap<>();
 
-        // hashset에 add해서 중복되는 원소 제거
+        // 중복되는 원소 제거
         for (int i = 0; i < logs.length; i++) {
             hs.add(logs[i]);
         }
 
         // hashmap에 문제 : 푼 사람의 수 put
         // 한문제 이상 푼 유저 add
-        for (String h : hs) {
-            String user = h.split(" ")[0];
-            String key = h.split(" ")[1];
+        for (String s : hs) {
+            String[] splitStirng = s.split(" ");
+            String user = splitStirng[0];
+            String key = splitStirng[1];
 
             users.add(user);
             hm.put(key, hm.getOrDefault(key, 0) + 1);
-
         }
 
-        int half = (int) Math.ceil((double) hs.size() / 2);
-        // ceil(올림) 내의 값은 double이어야 한다. hs.size는 int형이다.
-        // half: 참가자의 중간값. 1/2 = 1(☞0.5)
+        int wellKnownCount = 0;
+        if(users.size()%2 == 0){
+            wellKnownCount = users.size()/2;
+        }else {
+            wellKnownCount = users.size()/2 + 1;
+        }
 
         // 절반 이상이 푼 문제 구하기
-        for (String s : hm.keySet()) {
-            if (hm.get(s) >= half) {
-                answers.add(s);
+        Iterator<Entry<String, Integer>> iterator = hm.entrySet().iterator();
+        ArrayList<String> result = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Entry<String, Integer> entry = iterator.next();
+            if (entry.getValue() >= wellKnownCount){
+                result.add(entry.getKey());
             }
         }
-
         // list to array
-        answer = answers.toArray(new String[answers.size()]);
+        answer = result.toArray(new String[answers.size()]);
 
         return answer;
     }
--- practice/Level1/정류장.java
+++ practice/Level1/정류장.java
@@ -3,83 +3,36 @@ package Level1;
 import java.util.*;
 
 public class 정류장 {
+
     public static boolean[][] visited;
 
     public static void main(String[] args) {
-        int[][] bus_stop = {{1, 2}, {3, 3}};
-        int N = 3;
-        solution(N, bus_stop);
+        int[][] solution = solution(3, new int[][]{{1, 2}});
 
+        for (int[] v : solution) {
+            System.out.println(Arrays.toString(v));
+        }
     }
 
-    public static int[][] solution(int N, int[][] bus_stop) {
-        int[][] answer = new int[N][N];
+    private static int[][] solution(int N, int[][] bus_stop) {
+        int[][] distance = new int[N][N];
 
-        for (int i = 0; i < answer.length; i++){
-            Arrays.fill(answer[i], -1);
-        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int minValue = Integer.MAX_VALUE;
 
-        for (int i = 0; i < bus_stop.length; i++) {
-            answer[bus_stop[i][0] - 1][bus_stop[i][1] - 1] = 0;
-        }
+                for (int k = 0; k < bus_stop.length; k++) {
 
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < N; x++) {
-                if (answer[y][x] == -1) {
-                    int ret = BFS(y, x, 0, N, bus_stop);
-                    answer[y][x] = ret;
+                    int dx = Math.abs(i - (bus_stop[k][0] - 1));
+                    int dy = Math.abs(j - (bus_stop[k][1] - 1));
+
+                    int sum = dx + dy;
+
+                    distance[i][j] = minValue = Math.min(minValue, sum);
                 }
             }
         }
 
-        return answer;
-    }
-
-    //최단거리를 return 하는 BFS 함수
-    public static int BFS(int y, int x, int cnt, int N, int[][] bus_stop) {
-        // 상하좌우 배열
-        int[] direct_y = {-1, 1, 0, 0};
-        int[] direct_x = {0, 0, -1, 1};
-
-        Queue<node> q = new LinkedList<>();
-
-        visited = new boolean[N + 1][N + 1]; // 방문여부 체크
-        node node = new node(y, x, 0);
-        q.add(node);
-
-        while (!q.isEmpty()) {
-            node now = q.poll();
-
-            for (int t = 0; t < 4; t++) {
-                int dy = now.y + direct_y[t];
-                int dx = now.x + direct_x[t];
-
-                if (dy < 0 || dx < 0 || dy > N || dx > N) continue; // 범위 체크
-                if (visited[dy][dx] == true) continue; // 재방문 여부 체크
-                visited[dy][dx] = true;
-                q.add(new node(dy, dx, now.cnt + 1));
-
-                // 종료조건
-                for (int i = 0; i < bus_stop.length; i++) {
-                    if (dy == bus_stop[i][0] - 1 && dx == bus_stop[i][1] - 1) {
-                        return now.cnt + 1;
-                    }
-                }
-
-            }
-        }
-        return 0;
-    }
-
-    public static class node {
-        int y;
-        int x;
-        int cnt;
-
-        public node(int y, int x, int cnt) {
-            this.y = y;
-            this.x = x;
-            this.cnt = cnt;
-        }
+        return distance;
     }
 }
